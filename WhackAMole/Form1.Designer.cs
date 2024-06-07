40ecfa5ab67f733a963fca759ef3ee0a785da132
Tạ Đức Bảo <146197243+DucBaoUIT@users.noreply.github.com>
2024-06-07 21:31:25 +0700

    Add project files.

---
+++ WhackAMole/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WhackAMole
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 400);
+            this.tableLayoutPanel1.TabIndex = 0;
+
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(47, 13);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score: 0";
+
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Whack-a-Mole";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WhackAMole/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WhackAMole
+{
+    public partial class Form1 : Form
+    {
+        //Initialize
+        private int score = 0;
+        private const int maxScore = 30;
+        private Random rand = new Random();
+        private Button activeButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            //Setup 4 round for a game
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.ColumnCount = 2;
+
+            //Initialize Click on Layout
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Button btn = new Button();
+
+                    //Fill up screen and create click on Layout
+                    btn.Dock = DockStyle.Fill;
+                    btn.Click += new EventHandler(Button_Click);
+                    tableLayoutPanel1.Controls.Add(btn, j, i);
+                }
+            }
+
+            //Initialize Time 
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(Timer_Tick);
+            timer1.Start();
+
+            //Initialize Score
+            lblScore.Text = "Score: 0";
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            //Add point when click at right place
+            if (sender == activeButton)
+            {
+                //Plus
+                score++;
+                lblScore.Text = "Score: " + score;
+                activeButton.BackColor = default(Color);
+                activeButton = null;
+
+                //Reach 30 point will reset 
+                if (score >= maxScore)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Congratulations! You reached the maximum score!", "Game Over");
+                    ResetGame();
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (activeButton != null)
+            {
+                activeButton.BackColor = default(Color);
+            }
+            
+            //Random appearance
+            int row = rand.Next(2);
+            int col = rand.Next(2);
+            var control = tableLayoutPanel1.GetControlFromPosition(col, row);
+            if (control is Button button)
+            {
+                activeButton = button;
+                activeButton.BackColor = Color.Red;
+            }
+        }
+
+        private void ResetGame()
+        {
+            //Reset when reach 30
+            score = 0;
+            lblScore.Text = "Score: 0";
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.BackColor = default(Color);
+                }
+            }
+            timer1.Start();
+        }
+    }
+}
